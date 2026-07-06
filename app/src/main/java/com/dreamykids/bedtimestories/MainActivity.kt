e9b0a235e03ce802904f188a2e03c49f66d0b2ee
Sime <118187568+simeo2@users.noreply.github.com>
2026-07-06 23:02:34 +0200

    Format MainActivity Compose content

--- app/src/main/java/com/dreamykids/bedtimestories/MainActivity.kt
+++ app/src/main/java/com/dreamykids/bedtimestories/MainActivity.kt
@@ -6,4 +6,14 @@ import androidx.activity.compose.setContent
 import com.dreamykids.bedtimestories.navigation.DreamyNav
 import com.dreamykids.bedtimestories.ui.theme.DreamyTheme
 
-class MainActivity: ComponentActivity(){ override fun onCreate(savedInstanceState: Bundle?){ super.onCreate(savedInstanceState); setContent{ DreamyTheme{ DreamyNav() } } } }
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            DreamyTheme {
+                DreamyNav()
+            }
+        }
+    }
+}
